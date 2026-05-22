c424df272f512de87c12bc77ccebbea40b9a0207
Associated52 <100200982+Associated52@users.noreply.github.com>
2026-05-22 11:32:04 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -136,10 +136,13 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     const distPath = path.join(process.cwd(), "dist");
+    app.get('/sitemap.xml', (req, res) => {
+      res.sendFile(path.join(process.cwd(), 'sitemap.xml'));
+  });
     app.use(express.static(distPath));
     app.get("*", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
-    });
+      });
   }
 
   app.listen(PORT, "0.0.0.0", () => {
