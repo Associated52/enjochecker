3bd2d4a613edaa91052da4bbaa0bbf1a63eb59fe
Associated52 <100200982+Associated52@users.noreply.github.com>
2026-05-22 10:13:01 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -37,7 +37,6 @@ async function startServer() {
         "gemini-2.5-flash-lite",
         "gemini-2.5-flash",
         "gemini-1.5-flash",
-        "gemini-2.0-flash"
       ];
       const errorsList: { model: string; code: string; message: string }[] = [];
 
